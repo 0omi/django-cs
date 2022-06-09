bef57aa8890886051b26844df09c27a15c197f9b
cs314 <you@example.com>
2022-06-09 03:06:36 +0300

    aa

--- home/models.py
+++ home/models.py
@@ -4,9 +4,9 @@ import os
 # Create your models here.
 
 class Photo(models.Model):
-    name = models.CharField(max_length=50 , null=True  )
-    content = models.TextField(  null=True )
-    image = models.ImageField(upload_to='photos/%y/%m/%d' , null=True  )
+    name = models.CharField(max_length=50 , null=True   )
+    content = models.TextField(  null=True  )
+    image = models.ImageField(upload_to='photos/%y/%m/%d' , null=True   )
 
     def __str__(self):
         return self.name  # Function to make the images in the database with its name
